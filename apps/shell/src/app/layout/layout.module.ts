ca2607f0c14d001ef3f2a0cb632acd12c8e1139a
Linjith <linjith.kunnon@gmail.com>
2025-04-27 05:36:10 +0530

    refactoring

--- apps/shell/src/app/layout/layout.module.ts
+++ apps/shell/src/app/layout/layout.module.ts
@@ -1,66 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-
-// Angular Material Modules
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatTooltipModule } from '@angular/material/tooltip';
-
-// Component Imports
-import { HeaderComponent } from './header/header.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { PlaceholderComponent } from './placeholder/placeholder.component';
-
-// App Switcher Component
-import { AppSwitcherComponent } from './app-switcher/app-switcher.component';
-
-// Notification Component
-import { NotificationModule } from './notification/notification.module';
-
-// User Profile Component
-import { UserProfileModule } from './user-profile/user-profile.module';
-
-// Announcements Bar Component
-import { AnnouncementsBarComponent } from './announcements-bar/announcements-bar.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    SidenavComponent,
-    MainLayoutComponent,
-    BreadcrumbComponent,
-    PlaceholderComponent
+    
   ],
   imports: [
-    CommonModule,
-    RouterModule,
-    HttpClientModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
-    MatSlideToggleModule,
-    MatTooltipModule,
-    NotificationModule,
-    UserProfileModule,
-    // Standalone components
-    AppSwitcherComponent,
-    AnnouncementsBarComponent
   ],
   exports: [
-    MainLayoutComponent,
-    PlaceholderComponent
   ]
 })
 export class LayoutModule { }
